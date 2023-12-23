547bf9ac8cf73b37b03bd4242eb4fbd2d59c6e24
Rajwinder Mahal <rajwinder@mahal.dev>
2023-12-22 23:23:19 -0500

    Set up repo

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: true,
+}
